49d327a50e3c2ff214dc2c9d556b23b52b65defd
sungdune <sungjun527@gmail.com>
2024-04-28 10:26:49 +0900

    add model

---
+++ shrink_def.py
@@ -0,0 +1,24 @@
+import glob
+import json
+import os
+from typing import Dict
+from dataset.feature.feature import *
+from dataset import const
+
+path = 'data/feature_selection.json'
+with open(path, 'r') as f:
+    feature_selection = json.load(f)
+
+FEATURE_DEF_PATH = 'data/feature_definition'
+new_feature_def = dict()
+for file in glob.glob(f'{FEATURE_DEF_PATH}/*.json'):
+    with open(file, 'r') as f:
+        feature_def = json.load(f)
+    new_feature_def.update({
+        feature_name: feature 
+        for feature_name, feature in feature_def.items() 
+        if feature_name in feature_selection})
+len(new_feature_def)
+
+with open('data/feature_definition.json', 'w') as f:
+    json.dump(new_feature_def, f)
